e8298c69e07e69c85eba3895df2ddcd65f102c7e
doorwayder <doorwayder@gmail.com>
2021-02-18 10:06:11 +0300

    Search added in admin panel

--- gorgaz/dogovor/admin.py
+++ gorgaz/dogovor/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from .models import Dogovor
 
 
-admin.site.register(Dogovor)
+class DogovorAdmin(admin.ModelAdmin):
+    list_display = ('number', 'name', 'date', 'tel1', 'address_city', 'equip', )
+    list_display_links = ('number', 'name', )
+    list_filter = ('address_city',)
+    search_fields = ['name', 'address_city', 'number', 'tel1', 'tel2', 'tel3']
+
+
+admin.site.register(Dogovor, DogovorAdmin)
